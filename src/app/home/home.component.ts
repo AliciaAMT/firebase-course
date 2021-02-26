9617ed9251d89411e72e0a24adb7671e03762748
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2021-02-26 16:32:51 -0600

    Lesson 3.15 refactor to service module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
+import { CoursesService } from './../services/courses.service';
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
-import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
     selector: 'home',
@@ -16,18 +16,10 @@ export class HomeComponent implements OnInit {
     beginnersCourses$ : Observable<Course[]>;
     advancedCourses$ : Observable<Course[]>;
 
-    constructor(private db: AngularFirestore) {}
+    constructor(private CoursesService: CoursesService) {}
 
     ngOnInit() {
-      this.courses$ = this.db.collection('courses').snapshotChanges().pipe(map(snaps => {
-
-        return snaps.map(snap => {
-          return <Course> {
-            id: snap.payload.doc.id,
-            ...snap.payload.doc.data() as Course
-          }
-        });
-      }));
+      this.courses$ = this.CoursesService.loadAllCourses();
 
       this.beginnersCourses$ = this.courses$.pipe(
         map(courses => courses.filter(
@@ -42,6 +34,5 @@ export class HomeComponent implements OnInit {
           )
           )
           );
-
     }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,25 @@
+import { Course } from './../model/course';
+import { map } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  loadAllCourses(): Observable<Course[]> {
+    return this.db.collection('courses').snapshotChanges().pipe(map(snaps => {
+      return snaps.map(snap => {
+        return <Course> {
+          id: snap.payload.doc.id,
+          ...snap.payload.doc.data() as Course
+        }
+      });
+    }));
+  }
 }
