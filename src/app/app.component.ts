a21bb8d4f7b732f138ba0dde76c1dc8b02b2d0de
AliciaAMT <46018401+AliciaAMT@users.noreply.github.com>
2021-03-02 15:44:17 -0600

    32 - User Profile UI login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    isLoggedIn$: Observable<boolean>;
+    isLoggedOut$: Observable<boolean>;
 
+    constructor(private afAuth: AngularFireAuth) {
 
+    }
+    ngOnInit() {
+      this.afAuth.authState.subscribe(user => console.log(user));
+      this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
+      this.isLoggedOut$ = this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn));
+    }
     logout() {
-
+      this.afAuth.auth.signOut();
     }
 
 }
